4a38c43a8a6baac273c1154f089c7465c3851b6e
1-Pankaj <pankajisrani17@gmail.com>
2023-07-27 08:31:17 +0530

    Initial Commit

--- app/src/main/java/com/pankaj/askgpt_ai/MainActivity.kt
+++ app/src/main/java/com/pankaj/askgpt_ai/MainActivity.kt
@@ -2,12 +2,13 @@ package com.pankaj.askgpt_ai
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         if(resources.getString(R.string.mode) == "Day"){
             //day stuff
 
