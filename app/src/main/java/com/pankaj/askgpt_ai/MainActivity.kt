01487536dfcc89c1c2b54bdf1a87a6f38b8baa45
1-Pankaj <pankajisrani17@gmail.com>
2023-07-27 09:33:19 +0530

    Initial Commit

--- app/src/main/java/com/pankaj/askgpt_ai/MainActivity.kt
+++ app/src/main/java/com/pankaj/askgpt_ai/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import android.view.animation.AnimationUtils
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
@@ -18,14 +20,27 @@ class MainActivity : AppCompatActivity() {
         val illustration = findViewById<ImageView>(R.id.illustration)
         val textDesc = findViewById<TextView>(R.id.textDesc)
         val textPolicy = findViewById<TextView>(R.id.textPolicy)
+        val cardBack = findViewById<CardView>(R.id.cardBack)
+        val cardBackImage = findViewById<ImageView>(R.id.cardBackImage)
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val buttonContinue = findViewById<MaterialButton>(R.id.buttonContinue)
+        val cardContinue = findViewById<CardView>(R.id.cardContinue)
+
+        cardContinue.visibility = CardView.GONE
+        cardBack.visibility = CardView.GONE
+
 
         if(resources.getString(R.string.mode) == "Day"){
             //day stuff
             illustration.setImageResource(R.drawable.illustration)
+            cardBackImage.setImageResource(R.drawable.back)
+            emailEditText.setBackgroundResource(R.drawable.edittext)
         }
         else{
             //night stuff
             illustration.setImageResource(R.drawable.illustration_night)
+            cardBackImage.setImageResource(R.drawable.back_night)
+            emailEditText.setBackgroundResource(R.drawable.edittext_night)
         }
 
         val illustration_anim = AnimationUtils.loadAnimation(this, R.anim.illustration_anim)
@@ -40,6 +55,15 @@ class MainActivity : AppCompatActivity() {
             textDesc.startAnimation(fade_out)
             textPolicy.startAnimation(fade_out)
             buttonStart.startAnimation(fade_out)
+            fade_in.duration = 1500
+            cardBack.startAnimation(fade_in)
+            cardBack.visibility = CardView.VISIBLE
+            cardContinue.startAnimation(fade_in)
+            cardContinue.visibility = CardView.VISIBLE
+        }
+
+        cardBack.setOnClickListener(){
+
         }
     }
 }
